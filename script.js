73d347d7d809376d2b13d29a45aa957cdd7dc447
Pavan Badiger <pavnbadiger@gmail.com>
2026-06-16 17:01:42 +0530

    updated Dark/Light Mode Toggle

--- script.js
+++ script.js
@@ -87,3 +87,21 @@ window.addEventListener("mousemove", (e) => {
         `translate(${x}px, ${y}px)`;
 
 });
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        toggleBtn.textContent = "☀️";
+        localStorage.setItem("theme", "dark");
+    } else {
+        toggleBtn.textContent = "🌙";
+        localStorage.setItem("theme", "light");
+    }
+});
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    toggleBtn.textContent = "☀️";
+}
