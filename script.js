b2bd5ad8ca2fa40a9759c228cee4c050c7ef2d6f
Pavan Badiger <pavnbadiger@gmail.com>
2026-06-16 18:02:17 +0530

    updated Dark/Light Mode Toggle

--- script.js
+++ script.js
@@ -89,19 +89,21 @@ window.addEventListener("mousemove", (e) => {
 });
 const toggleBtn = document.getElementById("theme-toggle");
 
-toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
+if(localStorage.getItem("theme") === "light"){
+    document.body.classList.add("light-mode");
+    toggleBtn.innerHTML = '<i class="fas fa-sun"></i>';
+} else {
+    toggleBtn.innerHTML = '<i class="fas fa-moon"></i>';
+}
 
-    if (document.body.classList.contains("dark-mode")) {
-        toggleBtn.textContent = "☀️";
-        localStorage.setItem("theme", "dark");
-    } else {
-        toggleBtn.textContent = "🌙";
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    if(document.body.classList.contains("light-mode")){
         localStorage.setItem("theme", "light");
+        toggleBtn.innerHTML = '<i class="fas fa-sun"></i>';
+    } else {
+        localStorage.setItem("theme", "dark");
+        toggleBtn.innerHTML = '<i class="fas fa-moon"></i>';
     }
 });
-
-if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark-mode");
-    toggleBtn.textContent = "☀️";
-}
